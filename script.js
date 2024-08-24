e1f7db397a7ee2d109f5291842e3f3d91c15409e
Andrey <exxelgrant.13@mail.ru>
2024-08-24 08:47:35 +0000

    added iframe

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-const img=document.getElementById('DS')
-console.log(img)
-//img.addEventListner('click', (e) =>e.target.src='domofon+sputnik.html')
+setInterval(()=>{
+const iframe=document.getElementById('iframe')
+    fetch('http://92.43.189.69:8237/onvifsnapshot/media_service/snapshot?channel=1&subtype=2').then(data=>iframe.setAttribute('src',data.url))  
+  },2000) 
