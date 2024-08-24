0000b57e6b0331bd9a86ed63d37773b5c14d4586
Andrey <exxelgrant.13@mail.ru>
2024-08-24 08:56:02 +0000

    fixed iframe

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 setInterval(()=>{
-const iframe=document.getElementById('iframe')
-    fetch('http://92.43.189.69:8237/onvifsnapshot/media_service/snapshot?channel=1&subtype=2').then(data=>iframe.setAttribute('src',data.url))  
+const divFrame=document.getElementById('divFrame')
+  divFrame.innerHTML=''
+  const iframe=document.createElement('iframe')
+  iframe.setAttribute('src','http://92.43.189.69:8237/onvifsnapshot/media_service/snapshot?channel=1&subtype=2')
+  divFrame.appendChild(iframe)  
   },2000) 
