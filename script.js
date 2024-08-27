970a6a3e7faeb4ef403dec01f94aa878a8b8b5e8
Andrey <exxelgrant.13@mail.ru>
2024-08-27 13:50:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-setInterval(()=>{
-const divFrame=document.getElementById('divFrame')
-  divFrame.innerHTML=''
-  const iframe=document.createElement('iframe')
-  iframe.setAttribute('src','http://92.43.189.69:8237/onvifsnapshot/media_service/snapshot?channel=1&subtype=2')
-  divFrame.appendChild(iframe)  
-  },2000) 
+setInterval(() => {
+    const divFrame = document.getElementById('divFrame')
+    divFrame.innerHTML = ''
+    let image = document.createElement('img')
+    image.setAttribute('src', 'http://92.43.189.69:8237/onvifsnapshot/media_service/snapshot?channel=1&subtype=2')
+    image.setAttribute('width', '80%')
+    image.setAttribute('height', '80%')
+    image.setAttribute('alt', new Date())
+    divFrame.appendChild(image)
+  }, 3000) 
